3e679f994fec22844cfc6ede66fa5a4c0ce1f95a
Ryan L Nelson-Flack <rn01094@srcs.loc>
2017-01-26 13:52:14 -0800

    Changed Formatting

--- calculator_start.js
+++ calculator_start.js
@@ -1,31 +1,17 @@
-
-
-
-
 var current_input = "0";
 var memory = "0";
 var operator = 0;
 var div_by_zero_error = "Divide By Zero Error";
 var mode = "DEG";
-
-
-
-
 /**
-* Helper function for displaying the current input
-*/
-
+ * Helper function for displaying the current input
+ */
 function displayCurrentInput() {
     document.getElementById('screen').value = current_input;
 }
-
-
-
-
 /**
-* Adds a digit to the current input
-*/
-
+ * Adds a digit to the current input
+ */
 function addDigit(dig) {
     if (current_input.length < 18) {
         if ((eval(current_input) == 0) && (current_input.indexOf(".") == -1)) {
@@ -37,15 +23,9 @@ function addDigit(dig) {
     }
     displayCurrentInput();
 }
-
-
-
-
-
 /**
-* Adds a decimal to the current input
-*/
-
+ * Adds a decimal to the current input
+ */
 function addDecimal() {
     if (current_input.length == 0) {
         //no leading ".", use "0."
@@ -59,29 +39,18 @@ function addDecimal() {
     }
     displayCurrentInput();
 }
-
-
-
-
-
 /**
-* Clears everything.
-*/
-
+ * Clears everything.
+ */
 function allClear() {
     current_input = "0";
     operator = 0; //clear operator
     memory = "0"; //clear memory
     displayCurrentInput();
 }
-
-
-
-
 /**
-* Stores the last operator pushed for multiply, divide, add, or subtract.
-*/
-
+ * Stores the last operator pushed for multiply, divide, add, or subtract.
+ */
 function storeOperator(op) {
     if (op.indexOf("*") > -1) {
         operator = 1;
@@ -98,15 +67,9 @@ function storeOperator(op) {
     memory = current_input; //store value
     current_input = "0";
 }
-
-
-
-
-
 /**
-* Calculate using operator, the memory and what is current
-*/
-
+ * Calculate using operator, the memory and what is current
+ */
 function calculate() {
     if (operator == 1) {
         current_input = eval(memory) * eval(current_input);
@@ -129,133 +92,81 @@ function calculate() {
     memory = "0"; //clear memory
     displayCurrentInput();
 }
-
-
-
 /**
-* Change the sign of the current input
-*/
-
+ * Change the sign of the current input
+ */
 function changeSign() {
     current_input = current_input * (-1);
     displayCurrentInput();
 }
-
-
-
-
 /**
-* Clear the current input back to 0
-*/
-
+ * Clear the current input back to 0
+ */
 function clears() {
     current_input = "0";
     displayCurrentInput();
 }
-
-
-
-
 /**
-* Change the current input to a percentage
-*/
-
+ * Change the current input to a percentage
+ */
 function percentage() {
     current_input = current_input / 100;
     displayCurrentInput();
 }
-
-
-
-
 /**
-* Calculate the factorial of the current input
-*/
-
+ * Calculate the factorial of the current input
+ */
 function factorial() {
     for (i = current_input; i > 1; i--) {
         current_input = current_input * (i - 1);
     }
     displayCurrentInput();
 }
-
-
-
 /**
-* Calculate the square of the current input
-*/
-
+ * Calculate the square of the current input
+ */
 function square() {
     current_input = Math.pow(current_input, 2);
     displayCurrentInput();
 }
-
-
-
-
 /**
-* Calculate the square root of the current input
-*/
-
+ * Calculate the square root of the current input
+ */
 function squareRoot() {
     current_input = Math.sqrt(current_input);
     displayCurrentInput();
 }
-
-
-
 /**
 Calculate the inverse of the current input
 */
-
 function inverse() {
     current_input = 1 / current_input;
     displayCurrentInput();
 }
-
-
-
-
 /**
 Uses the current input in a sin function
 */
-
 function sin() {
     current_input = Math.sin(current_input);
     displayCurrentInput();
 }
-
-
-
-
 /**
 uses current input in cosine function
 */
-
 function cos() {
     current_input = Math.cos(current_input);
     displayCurrentInput();
 }
-
-
-
-
 /**
 uses curent input in tan function
 */
-
 function tan() {
     current_input = Math.tan(current_input);
     displayCurrentInput();
 }
-
-
-
-
 /**
-changes calculators mode to degree mode
+changes calculator mode to degree mode
 */
-
 function rad() {
     if (mode == "DEG") {
         //convert current input
@@ -264,13 +175,9 @@ function rad() {
     mode = "RAD";
     displayCurrentInput();
 }
-
-
-
 /**
-changes calculatos mode to radian mode
+changes calculator mode to radian mode
 */
-
 function deg() {
     if (mode == "RAD") {
         //convert current input
